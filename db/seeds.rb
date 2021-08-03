7d4f3d7c96631c73603e14424634fb2b27f81eb3
carlos <crmm17.cm@gmail.com>
2021-08-03 10:45:41 -0500

    Add task to optional seeds, initial seed and config
    Add config to separate production and development seeds, a task to run
    optional seeds and inital seed to populate database with initial user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+case Rails.env
+when "development"
+   puts "Seeding for env = development"
+   Dir[File.join(Rails.root, 'db', 'seeds', 'development', '*.rb')].sort.each { |seed| load seed }
+when "production"
+  puts "Seeding for env = production"
+  Dir[File.join(Rails.root, 'db', 'seeds', 'production', '*.rb')].sort.each { |seed| load seed }
+end
---
+++ db/seeds/production/init.rb
@@ -0,0 +1,2 @@
+# create initial user
+User.create(username: 'admin', email: 'admin@email.com', password: 'admin')
