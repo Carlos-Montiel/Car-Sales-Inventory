d39a36dad13fd7eba75cceab85b86621b8f84bea
carlos <crmm17.cm@gmail.com>
2021-08-03 12:56:21 -0500

    Fix seeds to create an admin user

--- db/seeds/development/init.rb
+++ db/seeds/development/init.rb
@@ -1,2 +1,2 @@
 # create initial user
-User.create!(username: 'admin', email: 'admin@email.com', password: '123456')
+User.create!(username: 'admin', email: 'admin@email.com', password: '123456', user_type: 'admin')
